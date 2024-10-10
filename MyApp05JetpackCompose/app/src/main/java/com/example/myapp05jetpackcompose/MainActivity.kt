656c5a6170e5b17335f19a48f616fe8af1d7d992
Jaroslav Hron <jaroslav.hron@tul.cz>
2024-10-10 11:12:01 +0200

    #4 zaklad appky

--- MyApp05JetpackCompose/app/src/main/java/com/example/myapp05jetpackcompose/MainActivity.kt
+++ MyApp05JetpackCompose/app/src/main/java/com/example/myapp05jetpackcompose/MainActivity.kt
@@ -3,45 +3,32 @@ package com.example.myapp05jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapp05jetpackcompose.ui.theme.MyApp05JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MyApp05JetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            ComposePerson()
         }
     }
 }
+/*
+Tato funkce definuje samotnou Composable, což je funkce
+v Jetpack Compose, která vykresluje UI.
+V tomto případě bude tato funkce obsahovat
+veškerou logiku a UI pro tuto jednoduchou aplikaci.
+ */
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ComposePerson() {
 
+}
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyApp05JetpackComposeTheme {
-        Greeting("Android")
-    }
+fun DefaultPreview() {
+    ComposePerson()
 }
