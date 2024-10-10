d9bd429982ebc0b84c914b85f6491b7b4dad31d7
Jaroslav Hron <jaroslav.hron@tul.cz>
2024-10-10 15:14:08 +0200

    #5

--- MyApp06MoreActivities/app/src/main/java/com/example/myapp06moreactivities/MainActivity.kt
+++ MyApp06MoreActivities/app/src/main/java/com/example/myapp06moreactivities/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.myapp06moreactivities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -9,12 +13,18 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnSecond = findViewById<Button>(R.id.btnSecond)
+        val etNickname = findViewById<EditText>(R.id.etNickname)
+
+        btnSecond.setOnClickListener{
+            val nickname = etNickname.text.toString()
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("NICKNAME", nickname)
+            startActivity(intent)
         }
+
     }
 }
--- MyApp06MoreActivities/app/src/main/java/com/example/myapp06moreactivities/SecondActivity.kt
+++ MyApp06MoreActivities/app/src/main/java/com/example/myapp06moreactivities/SecondActivity.kt
@@ -1,20 +1,32 @@
 package com.example.myapp06moreactivities
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SecondActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_second)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val tvInfo = findViewById<TextView>(R.id.twInfo)
+
+        val nickname = intent.getStringExtra("NICKNAME")
+
+        tvInfo.text = "Data z první aktivity. Jméno: $nickname"
+
+
+
+        val btnBack = findViewById<Button>(R.id.btnBack)
+        btnBack.setOnClickListener{
+            finish()
         }
     }
 }
