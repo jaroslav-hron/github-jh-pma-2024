7e6049b6cca50ea624d3d29cf83d7e1c34cfd3d5
jaroslav-hron <jaroslav.hron@tul.cz>
2024-12-22 00:26:49 +0100

    #13 update
    #13

--- MyApp015aSharedTasksDB/app/src/main/java/com/example/myapp015asharedtasksdb/MainActivity.kt
+++ MyApp015aSharedTasksDB/app/src/main/java/com/example/myapp015asharedtasksdb/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapp015asharedtasksdb.databinding.ActivityMainBinding
 import com.google.firebase.FirebaseApp
 import com.google.firebase.firestore.FirebaseFirestore
-
+ 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
